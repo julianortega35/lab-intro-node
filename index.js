19b06aba3852b097a05910468647ef64f583e1ea
julianortega <julianortega@MacBook-de-julianortega.local>
2020-07-21 12:15:44 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
 
-  add(item) {}
+    
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort ((a,b) => a - b);
+    this.length ++;
+    
+    
+  }
 
-  max() {}
+  get(pos) {
+    if (pos <= this.items.length && pos >= 0 ){
+      let number = this.items[pos]
+      return number;
+    } else {
+      throw new Error('OutOfBounds');
+      
+    }
 
-  min() {}
+  }
+  
+  
+   
+  max() {
+    if (!this.items.length == 0) {
+      let highNumber = this.items[this.items.length - 1]
+      return highNumber;
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  sum() {}
+  
+
+  min() {
+
+    if (!this.items.length == 0) {
+      let lowerNumber = this.items[0]
+      return lowerNumber;
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
+
+  sum() {
+    if (!this.items.length == 0) {
+    let sum = this.items.reduce((accumulator, currentValue) => accumulator + currentValue);
+    return sum
+  } else {
+    return 0;
+  }
+  }
 
   avg() {}
-}
 
+}
 module.exports = SortedList;
